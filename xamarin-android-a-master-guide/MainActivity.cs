3c037ce78014194fb3a99de70887bbf817bce433
Joachim Harms <jharms@aio.de>
2018-03-16 13:29:07 +0100

    Add project files.

---
+++ xamarin-android-a-master-guide/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace xamarin_android_a_master_guide
+{
+    [Activity(Label = "xamarin_android_a_master_guide", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
