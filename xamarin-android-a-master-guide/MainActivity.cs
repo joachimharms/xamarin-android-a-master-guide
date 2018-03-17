8c9f4a7482f812da11fad8e286867616c710dd71
joachimharms <joachimharms77@gmail.com>
2018-03-17 16:09:44 +0100

    Implement simple Button click

--- xamarin-android-a-master-guide/MainActivity.cs
+++ xamarin-android-a-master-guide/MainActivity.cs
@@ -1,18 +1,40 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Graphics;
 
 namespace xamarin_android_a_master_guide
 {
     [Activity(Label = "xamarin_android_a_master_guide", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        public bool isBlue = false;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+
+            button.Click += delegate
+            {
+                if (!isBlue)
+                {
+
+                    button.SetBackgroundColor(Color.Blue);
+                }
+                else
+                {
+
+                    button.SetBackgroundColor(Color.Red);
+                }
+                isBlue = !isBlue;
+            };
+
         }
     }
 }
--- xamarin-android-a-master-guide/Resources/Resource.Designer.cs
+++ xamarin-android-a-master-guide/Resources/Resource.Designer.cs
@@ -41,6 +41,22 @@ namespace xamarin_android_a_master_guide
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f040000
+			public const int myButton = 2130968576;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
