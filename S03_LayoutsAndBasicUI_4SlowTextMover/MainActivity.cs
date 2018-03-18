34fee8d6b59e04c5f68998afec1a48c775f032c6
joachimharms <joachimharms77@gmail.com>
2018-03-18 10:39:15 +0100

    Finish Sector 3 -done

--- S03_LayoutsAndBasicUI_4SlowTextMover/MainActivity.cs
+++ S03_LayoutsAndBasicUI_4SlowTextMover/MainActivity.cs
@@ -13,6 +13,20 @@ namespace S03_LayoutsAndBasicUI_4SlowTextMover
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+            TextView textView1 = FindViewById<TextView>(Resource.Id.textView1);
+            EditText editText1 = FindViewById<EditText>(Resource.Id.editText1);
+            Button button1 = FindViewById<Button>(Resource.Id.button1);
+
+            button1.Click += delegate
+            {
+                if (editText1.Text.Length > 0)
+                {
+                    string userInput = editText1.Text.ToString();
+                    textView1.Text += userInput[0];
+                    editText1.Text = userInput.Remove(0, 1);
+                }
+            };
+
         }
     }
 }
---
+++ S03_LayoutsAndBasicUI_5_SlowTextMover_Solution/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace S03_LayoutsAndBasicUI_5_SlowTextMover_Solution
+{
+    [Activity(Label = "S03_LayoutsAndBasicUI_5_SlowTextMover_Solution", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            TextView textView = FindViewById<TextView>(Resource.Id.textView1);
+            EditText editText = FindViewById<EditText>(Resource.Id.editText1);
+            Button button = FindViewById<Button>(Resource.Id.button1);
+
+            button.Click += delegate
+            {
+                textView.Text = string.Empty;
+
+                Task.Factory.StartNew(() => {
+                    while (editText.Text.Length > 0)
+                    {
+                        RunOnUiThread(() =>
+                        {
+                            var letter = editText.Text.Substring(0, 1);
+                            textView.Text += letter;
+                            editText.Text = editText.Text.Substring(1);
+
+                            // Thread.Sleep an dieser Stelle wird den UI Thread blockieren und die App abstürzen lassen.
+                            //Thread.Sleep(1000);
+
+                        });
+
+                        // Thread.Sleep() läuft nun auf einen Extrathread und die UI bleibt daher responsiv.
+                        Thread.Sleep(1000);
+                    }
+                });
+            };
+        }
+    }
+}
+
