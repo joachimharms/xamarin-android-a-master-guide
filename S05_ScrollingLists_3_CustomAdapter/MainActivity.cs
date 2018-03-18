66fce40fa6bffab4aae106d724cc4f45ad0eb3fe
joachimharms <joachimharms77@gmail.com>
2018-03-19 00:25:07 +0100

    Backup

---
+++ S05_ScrollingLists_3_CustomAdapter/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+
+namespace S05_ScrollingLists_3_CustomAdapter
+{
+    [Activity(Label = "S05_ScrollingLists_3_CustomAdapter", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var listView = FindViewById<ListView>(Resource.Id.listView1);
+            List<string> data = new List<string>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                data.Add(string.Format($"Item number: {i}"));
+            }
+
+            ArrayAdapter adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1, data);
+            listView.Adapter = adapter;
+
+            listView.ItemClick += ListView_ItemClick;
+        }
+
+        private void ListView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            Toast.MakeText(this, "Hallo Welt", ToastLength.Long).Show();
+        }
+    }
+}
+
---
+++ S05_ScrollingLists_3_CustomAdapter/MyAdapter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace S05_ScrollingLists_3_CustomAdapter
+{
+    public class MyAdapter : BaseAdapter
+    {
+
+        List<string> items;
+        Activity context;
+
+        public MyAdapter()
+        {
+
+        }
+
+
+        public override int Count => throw new NotImplementedException();
+
+        public override Java.Lang.Object GetItem(int position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long GetItemId(int position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
