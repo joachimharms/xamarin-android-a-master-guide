17652d3bbfee53d1adfc3c3c5d6a2466a5cdfc3e
joachimharms <joachimharms77@gmail.com>
2018-03-18 23:34:11 +0100

    Finish project ListView click handling -done

---
+++ S05_ScrollingLists_2_HandlingListViewClicks/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+using Android.Util;
+
+namespace S05_ScrollingLists_2_HandlingListViewClicks
+{
+    [Activity(Label = "S05_ScrollingLists_2_HandlingListViewClicks", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            List<string> data = new List<string>();
+            var listView = FindViewById<ListView>(Resource.Id.listView1);
+
+            for (int i = 0; i < 100; i++)
+            {
+                data.Add("Item number: " + i.ToString());
+            }
+
+            var adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1, data);
+
+            listView.Adapter = adapter;
+
+            listView.ItemClick += ListView_ItemClick;
+
+
+        }
+
+        private void ListView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            Log.Debug("ITEM CLICKED", string.Format($"Number: {e.Position}"));
+        }
+    }
+}
+
