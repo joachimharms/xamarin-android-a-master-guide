989583a6653523ab8737facc51477f5021e3bd4e
joachimharms <joachimharms77@gmail.com>
2018-03-18 15:11:22 +0100

    Finish project Spinner and Adapter -done

---
+++ S04_MoreBasicUI_3_Spinners/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace S04_MoreBasicUI_3_Spinners
+{
+    [Activity(Label = "S04_MoreBasicUI_3_Spinners", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var spinner = FindViewById<Spinner>(Resource.Id.spinner1);
+            ArrayAdapter adapter = ArrayAdapter.CreateFromResource(this, Resource.Array.planets_array, Android.Resource.Layout.SimpleSpinnerItem);
+            adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+
+            spinner.Adapter = adapter;
+
+            spinner.ItemSelected += delegate
+            {
+                // Business Logik hier
+            };
+
+        }
+    }
+}
+
