ba76d9d3a6aa5f951c5949d4386c63d646bddbf3
joachimharms <joachimharms77@gmail.com>
2018-03-18 15:28:24 +0100

    Start new project for own Solution for task -done

---
+++ S04_MoreBasicUI_4_Task_MySolution/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace S04_MoreBasicUI_4_Task_MySolution
+{
+    [Activity(Label = "S04_MoreBasicUI_4_Task_MySolution", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
