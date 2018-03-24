a984531b7fa814762a1f71549de814457c5cd9b4
joachimharms <joachimharms77@gmail.com>
2018-03-24 23:56:52 +0100

    Start new project graphics and drawables -done

---
+++ S06_Resources_4_ScreenDensityAndResolution/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace S06_Resources_4_ScreenDensityAndResolution
+{
+    [Activity(Label = "S06_Resources_4_ScreenDensityAndResolution", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var imageViewCar = FindViewById<ImageView>(Resource.Id.imageViewCar);
+            var buttonShowCar = FindViewById<Button>(Resource.Id.buttonShowCar);
+
+            buttonShowCar.Click += delegate 
+            {
+                imageViewCar.SetImageResource(Resource.Drawable.ic_car02_french);
+            };
+
+            
+        }
+    }
+}
+
