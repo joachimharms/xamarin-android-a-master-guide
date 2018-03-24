8a30288081a23f51441c5c36d093ad0f161abb5f
joachimharms <joachimharms77@gmail.com>
2018-03-24 14:09:20 +0100

    Finish project translation and countryspecific resources -done

---
+++ S06_Resources_2_Translation/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace S06_Resources_2_Translation
+{
+    [Activity(Label = "S06_Resources_2_Translation", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var buttonShowImage = FindViewById<Button>(Resource.Id.buttonShowImage);
+            var imageViewCar = FindViewById<ImageView>(Resource.Id.imageViewCar);
+
+            buttonShowImage.Click += delegate
+            {
+                imageViewCar.SetImageResource(Resource.Drawable.car01);
+                imageViewCar.SetScaleType(ImageView.ScaleType.CenterCrop);
+            };
+
+        }
+    }
+}
+
