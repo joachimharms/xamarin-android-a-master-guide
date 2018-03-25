c7302e2081e60856859299991ba4481a1d3b61e1
joachimharms <joachimharms77@gmail.com>
2018-03-25 12:03:39 +0200

    Transition XML files -done

--- S08_GraphicsAndAnimations_3_XmlTransitionAnimations/MainActivity.cs
+++ S08_GraphicsAndAnimations_3_XmlTransitionAnimations/MainActivity.cs
@@ -1,6 +1,8 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Transitions;
+using Android.Graphics.Drawables;
 
 namespace S08_GraphicsAndAnimations_3_XmlTransitionAnimations
 {
@@ -13,6 +15,14 @@ namespace S08_GraphicsAndAnimations_3_XmlTransitionAnimations
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            Button buttonTransition = FindViewById<Button>(Resource.Id.buttonTransition);
+
+            buttonTransition.Click += delegate
+            {
+                TransitionDrawable drawable = (TransitionDrawable)FindViewById<ImageView>(Resource.Id.imageViewPCTransition).Drawable;
+                drawable.StartTransition(3000);
+            };
         }
     }
 }
--- S08_GraphicsAndAnimations_3_XmlTransitionAnimations/Resources/Resource.Designer.cs
+++ S08_GraphicsAndAnimations_3_XmlTransitionAnimations/Resources/Resource.Designer.cs
@@ -41,11 +41,52 @@ namespace S08_GraphicsAndAnimations_3_XmlTransitionAnimations
 			}
 		}
 		
-		public partial class Layout
+		public partial class Drawable
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Main = 2130837504;
+			public const int c64 = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int thinkstation = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int transition_c64_thinkstation = 2130837506;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int buttonTransition = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int imageViewPCTransition = 2131034113;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
+		public partial class Layout
+		{
+			
+			// aapt resource value: 0x7f030000
+			public const int Main = 2130903040;
 			
 			static Layout()
 			{
@@ -60,8 +101,8 @@ namespace S08_GraphicsAndAnimations_3_XmlTransitionAnimations
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int app_name = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int app_name = 2130968576;
 			
 			static String()
 			{
