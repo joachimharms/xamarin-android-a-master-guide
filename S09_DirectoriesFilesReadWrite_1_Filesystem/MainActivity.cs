6c97a29cc3fc691c51244b428d89141cc1893772
joachimharms <joachimharms77@gmail.com>
2018-03-27 00:27:26 +0200

    initial commit new project filesystem

---
+++ S09_DirectoriesFilesReadWrite_1_Filesystem/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace S09_DirectoriesFilesReadWrite_1_Filesystem
+{
+    [Activity(Label = "S09_DirectoriesFilesReadWrite_1_Filesystem", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
